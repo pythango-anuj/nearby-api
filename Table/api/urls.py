13202435959dd9636a225bf1d661374126935b5d
Anuj2001 <2018034@iiitdmj.ac.in>
2020-11-11 21:30:40 +0530

    New Commit

--- Table/api/urls.py
+++ Table/api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+'''
 from .views import Nearbycreateapi,Nearbylistapi,Nearbyupdateapi,Nearbydeleteapi
 
 urlpatterns = [
@@ -7,3 +8,13 @@ urlpatterns = [
     path('api/<int:pk>',Nearbyupdateapi.as_view()),
     path('api/<int:pk>/delete',Nearbydeleteapi.as_view())   
 ]
+'''
+from api import views
+from rest_framework.urlpatterns import format_suffix_patterns
+
+urlpatterns = [
+    path('api/',views.NearbyapiList.as_view()),
+    path('api/<int:pk>/',views.NearbyapiDetail.as_view())
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- Table/api/views.py
+++ Table/api/views.py
@@ -1,3 +1,5 @@
+# Using generic class
+'''
 from django.shortcuts import render
 from .models import Nearby
 from .serializers import Nearbyserializers
@@ -20,3 +22,53 @@ class Nearbyupdateapi(generics.UpdateAPIView):
 class Nearbydeleteapi(generics.DestroyAPIView):
     queryset =  Nearby.objects.all()
     serializer_class = Nearbyserializers
+'''
+
+# Using APIview
+from .models import Nearby
+from .serializers import Nearbyserializers
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.http import Http404
+
+class NearbyapiList(APIView):
+    def get(self,request,format = None):
+        queries = Nearby.objects.all()
+        serializer = Nearbyserializers(queries,many = True)
+        return Response(serializer.data)
+
+    def post(self,request,format = None):
+        serializer = Nearbyserializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        #return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+class NearbyapiDetail(APIView):
+    """
+    Retrieve, update or delete a snippet instance.
+    """
+    def get_object(self, pk):
+        try:
+            return Nearby.objects.get(pk=pk)
+        except Nearby.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        query = self.get_object(pk)
+        serializer = Nearbyserializers(query)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        query = self.get_object(pk)
+        serializer = Nearbyserializers(query, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        query = self.get_object(pk)
+        query.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
